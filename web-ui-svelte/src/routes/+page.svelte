8052cd6809f00fa01f7a246e2ce1509cf5d41712
Esaias Cronelius <esaias@cronelius.se>
2023-06-16 17:01:30 +0200

    Added header for Web UI

--- web-ui-svelte/src/routes/+page.svelte
+++ web-ui-svelte/src/routes/+page.svelte
@@ -1,26 +1,8 @@
-<svelte:head>
-  <title>Home</title>
-  <meta name="description" content="Svelte demo app" />
-</svelte:head>
+<script lang="ts">
+  import Header from '../components/Header.svelte'
+</script>
 
-<section>
-  <h1> Welcome to your new SvelteKit app </h1>
-
-  <h2>
-    try editing <strong>src/routes/+page.svelte</strong>
-  </h2>
-</section>
+<Header />
 
 <style>
-  section {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    flex: 0.6;
-  }
-
-  h1 {
-    width: 100%;
-  }
 </style>
