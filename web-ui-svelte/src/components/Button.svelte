70986e7c159813d6d95be05b88d85c1d46c25b0d
Esaias Cronelius <esaias@cronelius.se>
2023-06-18 17:28:42 +0200

    Authentication form design

---
+++ web-ui-svelte/src/components/Button.svelte
@@ -0,0 +1,26 @@
+<button on:click><slot /></button>
+
+<style>
+  button {
+    display: block;
+    width: 100%;
+    padding: 0.5rem;
+    font-size: 1rem;
+    line-height: 1.25;
+    color: #fff;
+    background-color: #3273dc;
+    border: 1px solid transparent;
+    border-radius: 0.25rem;
+    transition: background-color 0.15s ease-in-out, border-color 0.15s ease-in-out;
+  }
+
+  button:hover {
+    background-color: #2767d8;
+    border-color: #2767d8;
+  }
+
+  button:focus {
+    outline: 0;
+    box-shadow: 0 0 0 0.125em rgba(50, 115, 220, 0.25);
+  }
+</style>
---
+++ web-ui-svelte/src/routes/authentication/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Button from '../../components/Button.svelte'
+  import Input from '../../components/Input.svelte'
+
+  let password: string = ''
+  const onSubmit = () => console.log(password)
+</script>
+
+<div class="form">
+  <Input label="Password" bind:value={password} {onSubmit} />
+  <Button on:click={onSubmit}>Sign in</Button>
+</div>
+
+<style>
+  .form {
+    max-width: 20rem;
+    margin: 3rem auto 0 auto;
+  }
+</style>
